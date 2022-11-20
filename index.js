9778f73cf5a3211de9d28704ce6d425d3b684961
brent couillard <brentcouillard@yahoo.com>
2022-11-20 01:33:40 -0600

    commit tree bc9b5561e7e0755b55b362cc4b8a938d5433ecb5

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your code here
+
+
+
+
+
+function submitData(name,email) {
+    const configureObject = {
+        method: "POST",
+        headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+              "name": name,
+              "email": email,
+            })
+
+    }
+    return fetch("http://localhost:3000/users", configureObject)
+        .then(resp => resp.json())
+        .then(newUser => document.body.innerHTML = newUser["id"])
+        .catch(error => document.body.innerHTML = error.message)
+}
+
+
+
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json",
+//     },
+//     body: JSON.stringify({
+//       name: "Brent",
+//       email: "brentsemail",
+//     }),
+//   };
+  
+//   fetch("http://localhost:3000/users", submitData);
